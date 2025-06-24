bb1718307e9c0086529af9fdd5cc3f6cd2a23df0
veee <paulag10987@gmail.com>
2025-06-24 13:46:58 +0800

    Update #20.py

--- EasyLC/LC#20/#20.py
+++ EasyLC/LC#20/#20.py
@@ -1,16 +1,19 @@
 class Solution(object):
     def isValid(self, s):
+        
         stack = []
         pair = {')': '(', ']': '[', '}': '{'}
 
         for char in s:
             
-            if char in pair.values():  # opening brackets
+            if char in pair.values():
                 stack.append(char)
             
-            elif char in pair:  # closing brackets
+            elif char in pair:  
+                
                 if not stack or stack[-1] != pair[char]:
                     return False
+                    
                 stack.pop()
         
         return not stack
